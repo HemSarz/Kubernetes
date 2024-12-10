b969f9327bb75c2db713410297a42d8316ab56ec
Hemen Sarzali <hemen.sarzali@hotmail.com>
2024-12-10 12:18:19 +0100

    Added cmdlet to verify perm assignments

--- terraform/fic/spnconfig.ps1
+++ terraform/fic/spnconfig.ps1
@@ -1,48 +1,44 @@
-############################################ 
-# Set Azure SPN | Permissions VB
-############################################
-
 $MSGraphApi = "00000003-0000-0000-c000-000000000000" # MS Graph API Id
-$appDirRoleId = "19dbc75e-c2e2-444c-a770-ec69d8559fc7=Role" # Directory.ReadWrite.All
+$permissions = @{
+    "Directory.ReadWrite.All"   = "19dbc75e-c2e2-444c-a770-ec69d8559fc7=Role"
+    "Application.ReadWrite.All" = "1bfefb4e-e0b5-418b-a88f-73c46d2cc8e9=Role"
+}
 
-# Existing SPN name
+$fields = @("appId", "id")
 $backend_spn = "tfazspn"
 
-##################################################################
-# Retrieve existing Service Principal credentials
-##################################################################
+$spninfo = @{ }
+foreach ($field in $fields) {
+    $spninfo[$field] = az ad sp show --id $(az ad sp list --display-name $backend_spn --query [0].appId -o tsv) --query $field -o tsv
+}
 
-Write-Host "Using existing service principal..." -ForegroundColor Yellow
-
-# Retrieve the SPN appId (client ID) for the existing SPN
-$backend_SPNappId = $(az ad sp list `
-    --display-name $backend_spn `
-    --query [0].appId `
-    -o tsv)
-
-# Fetch SPN object ID
-$backend_SPNObjId = $(az ad sp show `
-    --id $backend_SPNappId `
-    --query id `
-    -o tsv)
+$backend_SPNappId = $spninfo["appId"]
+$backend_SPNObjId = $spninfo["id"]
 
 Write-Host "Assign SPN AD Permissions..." -ForegroundColor Yellow
 
-# Assign Directory permissions
-Write-Host 'Assign permission appDir...' -ForegroundColor Green
-az ad app permission add `
-    --id $backend_SPNappId `
-    --api $MSGraphApi `
-    --api-permissions $appDirRoleId
+foreach ($permission in $permissions.GetEnumerator()) {
+    Write-Host "Assigning permission: $($permission.Key)" -ForegroundColor Green
+    az ad app permission add `
+        --id $backend_SPNappId `
+        --api $MSGraphApi `
+        --api-permissions $permission.Value `
+        -o table
 
-# Grant permissions to the SP
-Write-Host 'Granting permissions...' -ForegroundColor Green
-az ad app permission grant `
-    --id $backend_SPNappId `
-    --api $MSGraphApi `
-    --scope $appDirRoleId
+    Write-Host "Granting permission: $($permission.Key)" -ForegroundColor Green
+    az ad app permission grant `
+        --id $backend_SPNappId `
+        --api $MSGraphApi `
+        --scope $permission.Value `
+        -o table    
+}
 
-# Admin consent
+Start-Sleep 20
+
+# Admin consent after all permissions have been added
 Write-Host 'Granting admin-consent' -ForegroundColor Green
 az ad app permission admin-consent `
     --id $backend_SPNappId
+
+Find-MgGraphPermission Directory.ReadWrite.All | Format-List
+Find-MgGraphPermission Application.ReadWrite.All | Format-List
