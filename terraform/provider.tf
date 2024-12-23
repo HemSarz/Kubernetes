d7a26f0640369654994ed69673cdf272d03b51c4
Hemen Sarzali <hemen.sarzali@hotmail.com>
2024-12-23 13:47:33 +0100

    Added req providers blocks

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,24 +1,35 @@
 terraform {
+  backend "azurerm" {
+    resource_group_name  = var.state_resource_group_name
+    storage_account_name = var.state_storage_account_name
+    container_name       = "tfstatebckndcont"
+    key                  = "tfstatebcknd"
+  }
+
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.14.0"
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.3"
     }
+
 
     azuread = {
       source  = "hashicorp/azuread"
-      version = "3.0.2"
+      version = "2.50.0"
     }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.3"
+    github = {
+      source  = "integrations/github"
+      version = "6.2.1"
     }
-
 
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
 }
