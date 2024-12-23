39bfbf36f05ccb018e02802272d480c8dafbc085
Hemen Sarzali <hemen.sarzali@hotmail.com>
2024-12-23 13:10:20 +0100

    renamed data block for spn to match fedaccess.tf value used

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,6 +2,6 @@ data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 data "azuread_client_config" "current" {}
 
-data "azuread_application" "current" {
+data "azuread_application" "tfazspn" {
   display_name = "tfazspn"
 }
