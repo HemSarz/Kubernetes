be6b130dc8843877b8f139dac5452b505ca5f11a
Hemen Sarzali <hemen.sarzali@hotmail.com>
2024-12-23 21:04:41 +0100

    Added backend config

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,29 +1,29 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.state_resource_group_name
-    storage_account_name = var.state_storage_account_name
-    container_name       = "tfstatebckndcont"
+    resource_group_name  = "tfaz-bcknd-rg"
+    storage_account_name = "backndtfazstg01"
+    container_name       = "tfstatebcknd"
     key                  = "tfstatebcknd"
   }
-
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.6.3"
-    }
+}
 
 
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "2.50.0"
-    }
-
-    github = {
-      source  = "integrations/github"
-      version = "6.2.1"
-    }
-
+required_providers {
+  random = {
+    source  = "hashicorp/random"
+    version = ">= 3.6.3"
   }
+
+  azuread = {
+    source  = "hashicorp/azuread"
+    version = "2.50.0"
+  }
+
+  github = {
+    source  = "integrations/github"
+    version = "6.2.1"
+  }
+
 }
 
 provider "azurerm" {
