7a28dc4652c0722c44582c0050fc7d065c89c6a3
Hemen Sarzali <hemen.sarzali@hotmail.com>
2024-12-23 13:08:00 +0100

    added data for spn

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,6 +2,6 @@ data "azurerm_client_config" "current" {}
 data "azurerm_subscription" "current" {}
 data "azuread_client_config" "current" {}
 
-data "azuread_service_principal" "current" {
+data "azuread_application" "current" {
   display_name = "tfazspn"
 }
