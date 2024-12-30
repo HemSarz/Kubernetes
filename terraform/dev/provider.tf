1f72331bcbf3d1553e868d89d34aadda11252898
Hemen Sarzali <hemen.sarzali@hotmail.com>
2024-12-30 09:33:34 +0100

    Changed stg name

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfaz-bcknd-rg"
-    storage_account_name = "backndtfazstg01"
+    storage_account_name = "bckndstg"
     container_name       = "tfstatebcknd"
     key                  = "tfstatebcknd"
   }
