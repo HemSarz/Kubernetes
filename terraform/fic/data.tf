d33a747fb1c5a0d967159eebce35d532425900d6
Hemen Sarzali <hemen.sarzali@hotmail.com>
2024-12-30 10:03:31 +0100

    rm k8s ref

--- terraform/fic/data.tf
+++ terraform/fic/data.tf
@@ -13,15 +13,3 @@ data "azuread_service_principal" "tfazspnName" {
 data "azuread_service_principal" "tfazspn" {
   object_id = data.azuread_service_principal.tfazspnName.object_id
 }
-
-#Get the current version of the Kubernetes service
-data "azurerm_kubernetes_service_versions" "current" {
-  location        = azurerm_resource_group.rg-dev.location
-  include_preview = false
-}
-
-# AKSAdminGroupMembers
-
-data "azuread_user" "gb" {
-  user_principal_name = "gb@hemensarzalihotmail.onmicrosoft.com"
-}
