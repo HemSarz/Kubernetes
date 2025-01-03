5c2ec3160f0ec88f50ce98747ed8ede0f2c680b9
Hemen Sarzali <hemen.sarzali@hotmail.com>
2025-01-03 08:03:05 +0100

    Removed acr. Using docker hub instead

--- terraform/dev/acr.tf
+++ terraform/dev/acr.tf
@@ -1,4 +1,4 @@
-resource "azurerm_container_registry" "acr" {
+/*resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg-dev.name
   location            = azurerm_resource_group.rg-dev.location
@@ -8,3 +8,4 @@ resource "azurerm_container_registry" "acr" {
     Environment = var.env
   }
 }
+*/
--- terraform/dev/aks.tf
+++ terraform/dev/aks.tf
@@ -56,8 +56,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
+/*
 resource "azurerm_role_assignment" "acr_role_assignment" {
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name = "AcrPull"
   scope                = azurerm_container_registry.acr.id
 }
+*/
